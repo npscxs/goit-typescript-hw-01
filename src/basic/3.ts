8fe78e9e5d35ba9fdde203d2ea1f2e3a865d8aa6
npscxs <npscxs@gmail.com>
2025-04-25 15:28:18 +0200

    rework task 3

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -3,3 +3,6 @@ unionType = "Union type";
 console.log("task-basic-3:", unionType);
 unionType = 15;
 console.log("task-basic-3:", unionType);
+let status: "enable" | "disable";
+console.log("task-basic-3:", (status = "enable"));
+console.log("task-basic-3:", (status = "disable"));
